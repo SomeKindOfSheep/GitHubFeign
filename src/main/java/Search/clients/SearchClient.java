296c346ca42e5ed4c46ca630a9e8831d10eece7a
Hendrik De Beer <Hendrik.debeer92@gmail.com>
2021-04-11 21:31:44 +0200

    Using jackson I can now grab JSON

--- src/main/java/Search/clients/SearchClient.java
+++ src/main/java/Search/clients/SearchClient.java
@@ -1,15 +1,21 @@
 package Search.clients;
 
 import Search.models.SearchResult;
+import com.fasterxml.jackson.databind.JsonNode;
 import feign.*;
 
 import java.util.Map;
 
 public interface SearchClient {
 
+    /*@RequestLine("GET api/as/v1/engines/sa-faq-feed/search")
+    @Body("%7B\"query\": \"{searchParam}\"%7D")
+    SearchResult findBySearchQuery(@HeaderMap Map<String, String> headerMap, @Param("searchParam") String searchParam);*/
+
     @RequestLine("GET api/as/v1/engines/sa-faq-feed/search")
     @Body("%7B\"query\": \"{searchParam}\"%7D")
-    SearchResult findBySearchQuery(@HeaderMap Map<String, String> headerMap, @Param("searchParam") String searchParam);
+    //TODO limit search query
+    JsonNode findBySearchQuery(@HeaderMap Map<String, String> headerMap, @Param("searchParam") String searchParam);
 
 }
 
--- src/main/java/Search/controllers/SearchController.java
+++ src/main/java/Search/controllers/SearchController.java
@@ -3,10 +3,13 @@ package Search.controllers;
 
 import Search.clients.SearchClient;
 import Search.models.Result;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Feign;
 import feign.gson.GsonDecoder;
-import feign.gson.GsonEncoder;
 import feign.jackson.JacksonDecoder;
+
 
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -15,12 +18,46 @@ import java.util.Map;
 public class SearchController {
 
     public static void main(String... args) {
+        /*ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.findAndRegisterModules();*/
 
-        SearchClient search = Feign.builder()
-                .decoder(new GsonDecoder())
+
+        SearchClient searchClient = Feign.builder()
+                // TODO use jackson decoder
+                // TODO limit what returns
+                // can we trick the deserializer to just return the object
+                .decoder(new JacksonDecoder())
                 .target(SearchClient.class, "https://dsbggena115v.standardbank.co.za:443/");
 
+
         Map<String,String> headermap = new LinkedHashMap<>();
+        headermap.put("Content-Type","application/json");
+        headermap.put("Authorization","Bearer search-a8t5kmo7nvg72rzhv4nz91my");
+        JsonNode node = searchClient.findBySearchQuery(headermap, "lotto");
+        JsonNode result = node.get("results");
+        if (result.isArray()) {
+            for (JsonNode resultNode : result) {
+                System.out.println(resultNode);
+            }
+        }
+
+
+        /*System.out.println(node.get("results").get(0).get("question").get("raw"));
+        System.out.println(node.get("results").get(0).get("answer").get("raw"));
+        System.out.println(node.get("results").get(0).get("faqs_order").get("raw"));
+        System.out.println(node.get("results").get(0).get("category_title").get("raw"));
+        System.out.println(node.get("results").get(0).get("category_order").get("raw"));
+        System.out.println(node.get("results").get(0).get("faqs_title").get("raw"));
+        System.out.println(node.get("results").get(0).get("faqs_id").get("raw"));
+        System.out.println(node.get("results").get(0).get("basic_title").get("raw"));
+        System.out.println(node.get("results").get(0).get("basic_id").get("raw"));
+        System.out.println(node.get("results").get(0).get("faq_id").get("raw"));
+        System.out.println(node.get("results").get(0).get("faq_title").get("raw"));*/
+
+
+
+       /*Map<String,String> headermap = new LinkedHashMap<>();
         headermap.put("Content-Type","application/json");
         headermap.put("Authorization","Bearer search-a8t5kmo7nvg72rzhv4nz91my");
         List<Result> listOfSearchResults = search.findBySearchQuery(headermap, "Lotto").getResults();
@@ -28,7 +65,7 @@ public class SearchController {
             System.out.println("------------");
             System.out.println("Question: "+ result.getQuestion().getRaw());
             System.out.println("Answer: "+result.getAnswer().getRaw());
-        }
+        }*/
 
     }
 }
